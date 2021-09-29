cc637b97bbd80208eaeea9d5cdca9ef8d7f60052
Martti Malmi <sirius@iki.fi>
2021-09-29 21:21:26 +0300

    metrics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import Gun from 'gun';
@@ -20,12 +19,20 @@ class App extends Component {
         }
       },
     }).then(ipfs => {
+      window.ipfs = ipfs;
       this.gun = new Gun({ipfs});
       this.gun.get('messages').map().on((msg, id) => {
         const messages = this.state.messages;
         messages[id] = msg;
         this.setState({messages});
       });
+
+      setInterval(() => {
+        ipfs.pubsub.peers('gundb').then(peers => {
+          document.getElementById('topic-peers').innerText = 'Pubsub topic peers: ' + peers.length;
+        });
+        document.getElementById('network-peers').innerText = 'Network peers: ' + ipfs.libp2p.metrics.peers.length;
+      }, 1000);
     });
   }
 
@@ -43,14 +50,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>Chat over Gun & IPFS Pubsub</h1>
-          <a href="https://github.com/mmalmi/gun-ipfs-chat">Source</a>
+          <a className="App-link" href="https://github.com/mmalmi/gun-ipfs-chat">Source</a>
+          <div id="network-peers">
+            Network peers: 0
+          </div>
+          <div id="topic-peers">
+            Pubsub topic peers: 0
+          </div>
+          <small>Usually takes a while to find topic peers</small>
+          <br/>
           <form onSubmit={e => this.onSubmit(e)}>
             <input id="msg-input" placeholder="Type a message" />
             <input type="submit" value="Send" />
-          </form>
+          </form><br/>
           {Object.keys(this.state.messages).map(key =>
             <div key={key}>{this.state.messages[key]}</div>
-          )}
+          ).reverse()}
         </header>
       </div>
     );
